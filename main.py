58b1039bea9082e060dd7dea7722e58e026b2d1e
Andres19011 <agallegogarcia030@gmail.com>
2025-08-17 00:31:41 -0500

    feature: agregue nueva clase BaseModel

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "hello, World"}
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    stock: bool = True 
+
+fake_bd = []
